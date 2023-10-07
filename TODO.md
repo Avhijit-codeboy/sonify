a650f48b8dc6e72ba9efe8efd534875899c5eb0a
Dheeraj Shenoy <dheerajshenoy22@gmail.com>
2023-10-07 11:15:47 +0530

    added resources file

---
+++ TODO.md
@@ -0,0 +1,3 @@
+1. [x] Open file
+2. [ ] Handle different file formats
+3. [x] Handle no file selection
--- src/GUI.py
+++ src/GUI.py
@@ -19,11 +19,13 @@ import scipy as sp
 import cv2
 import sounddevice as sd
 import time
+import threading
+import resources
 
 # Global Variables
-ICON_DIR = "../icons/"
-IMAGE_DIR = "../images/"
+IMAGE_DIR = "images/"
 
+# Main Window class
 class MainWindow(QMainWindow):
     def __init__(self, parent = None):
         super().__init__()
@@ -38,14 +40,14 @@ class MainWindow(QMainWindow):
 
     # Function for creating the drawer
     def CreateDrawer(self):
-    
+
         # Drawer Widget
         self.drawer = QWidget()
 
         self.drawerLayout = QGridLayout()
         self.traverseLabel = QLabel("Traversing")
         self.traverseLabel.setToolTip("Method of moving through the image")
-        
+
         # Travers Combo Box
         self.traverseComboBox = QComboBox()
 
@@ -58,7 +60,7 @@ class MainWindow(QMainWindow):
 
         # self.drawerLayout.addWidget(QLabel("Drawer"))
         self.drawer.setLayout(self.drawerLayout)
-    
+
         # Map Label
         self.mapLabel = QLabel("Mapping")
         self.mapLabel.setToolTip("Method of mapping the feature of image to parameters of sound")
@@ -68,11 +70,11 @@ class MainWindow(QMainWindow):
         self.mapComboBox.addItem("Color to Frequency")
 
         # Play Button
-        self.playButton = QPushButton(QIcon(ICON_DIR + "icon_play.png"), "")
+        self.playButton = QPushButton(QIcon(":/icons/play.png"), "")
         self.playButton.setToolTip("Play")
         self.playButton.setEnabled(False)
-        self.playButton.clicked.connect(self.PlayAudio)
-    
+        self.playButton.clicked.connect(self.Play)
+
         # Drawer Layout
         self.drawerLayout.addWidget(self.traverseLabel, 0, 0)
         self.drawerLayout.addWidget(self.traverseComboBox, 0, 1)
@@ -88,13 +90,15 @@ class MainWindow(QMainWindow):
         # TODO: handle ask on save
         QCoreApplication.exit(0)
 
-
     # Function to handle opening files
     def FileOpen(self, fileName = None):
         if fileName is None:
             self.file = QFileDialog.getOpenFileName(self, "Open File",
                                                     filter = "Image Files (*.jpg *.jpeg *.png *.tiff *.hdf5 *.fits)")[0]
-            
+            if self.file == "":
+                self.Msg("Please select an image", 10)
+                return
+
             self.Msg("Loading Image", 2)
             self.ReadImage()
             self.Msg("Image Loaded", 5)
@@ -108,7 +112,7 @@ class MainWindow(QMainWindow):
         thresholds = [26 , 52 , 78 , 104,  128 , 154 , 180]
         note = scale_freqs[0]
         if (h <= thresholds[0]):
-             note = scale_freqs[0]
+            note = scale_freqs[0]
         elif (h > thresholds[0]) & (h <= thresholds[1]):
             note = scale_freqs[1]
         elif (h > thresholds[1]) & (h <= thresholds[2]):
@@ -137,7 +141,7 @@ class MainWindow(QMainWindow):
         self.InitStatusBar()
 
         self.setCentralWidget(self.mainWidget)
-    
+
     # Initialisation function for the canvas
     def InitCanvas(self):
         self.figure = Figure(figsize=(10, 10))
@@ -158,21 +162,20 @@ class MainWindow(QMainWindow):
 
         # File Menu Buttons
 
-        self.action_file_open = QAction(QIcon(ICON_DIR + "icon_open.png"), "Open", self)
+        self.action_file_open = QAction(QIcon(":/icons/open.png"), "Open", self)
         self.action_file_open.setShortcut("Ctrl+O")
 
-        self.action_file_exit = QAction(QIcon(ICON_DIR + "icon_exit.png"), "Exit", self)
+        self.action_file_exit = QAction(QIcon(":/icons/exit.png"), "Exit", self)
 
         self.action_file_open.triggered.connect(lambda: self.FileOpen(None))
         self.action_file_exit.triggered.connect(self.Exit)
-        self.aboutMenu.triggered.connect(lambda: print("DD"))
 
         self.fileMenu.addAction(self.action_file_open)
         self.fileMenu.addAction(self.action_file_exit)
 
         # Edit Menu Buttons
 
-        self.action_edit_prefs = QAction(QIcon(ICON_DIR + "icon_prefs.png"), "Preferences", self)
+        self.action_edit_prefs = QAction(QIcon(":icons/prefs.png"), "Preferences", self)
 
         self.editMenu.addAction(self.action_edit_prefs)
         self.setMenuBar(self.menubar)
@@ -183,7 +186,7 @@ class MainWindow(QMainWindow):
         self.statusbar_layout = QHBoxLayout()
         self.setStatusBar(self.statusbar)
         self.mainLayout.addWidget(self.statusbar)
-        
+
         self.statusbar.setMaximumHeight(20)
 
         self.status_fileName = QLabel("File Name")
@@ -207,20 +210,27 @@ class MainWindow(QMainWindow):
         self.mainSplitter.addWidget(self.canvas)
         self.mainLayout.addWidget(self.mainSplitter)
 
+    # Function that handles the play button click
+    def Play(self):
+        # self.PlayAudio()
+        self.MoveHorizLine()
+
     # Function to play audio
     def PlayAudio(self):
         sd.play(self.song)
         time.sleep(5)
         sd.stop()
 
+    def MoveHorizLine(self):
+        pass
+
     # Function for reading the image
     def ReadImage(self):
-        
         self.img = Image.open(self.file)
         self.imghsv = cv2.cvtColor(np.array(self.img), cv2.COLOR_BGR2HSV)
         self.ax.imshow(self.img)
         self.UpdateCanvas()
-    
+
         # Update the statusbar info
         self.status_fileName.setText(self.file)
         self.status_fileSize.setText(self.FileSize())
@@ -268,7 +278,7 @@ class MainWindow(QMainWindow):
     # Helper function to update canvas
     def UpdateCanvas(self):
         self.canvas.draw()
-    
+
     # def map_stack(self, skipw, skiph):
     #     for i in range(0, self.img_height, int(self.img_height / skiph)):
     #         for j in range(0, self.img_width, int(self.img_width / skipw)):
@@ -288,22 +298,21 @@ class MainWindow(QMainWindow):
     #             hue = imghsv[i][j][0]
     #             self.hues.append(hue)
 
+    def Get_piano_notes(self):   
+        # White keys are in Uppercase and black keys (sharps) are in lowercase
+        octave = ['C', 'c', 'D', 'd', 'E', 'F', 'f', 'G', 'g', 'A', 'a', 'B'] 
+        base_freq = 440 #Frequency of Note A4
+        keys = np.array([x+str(y) for y in range(0,9) for x in octave])
+        # Trim to standard 88 keys
+        start = np.where(keys == 'A0')[0][0]
+        end = np.where(keys == 'C8')[0][0]
+        keys = keys[start:end+1]
 
+        note_freqs = dict(zip(keys, [2**((n+1-49)/12)*base_freq for n in range(len(keys))]))
+        note_freqs[''] = 0.0 # stop
+
+        return note_freqs
 """
-def get_piano_notes():   
-    # White keys are in Uppercase and black keys (sharps) are in lowercase
-    octave = ['C', 'c', 'D', 'd', 'E', 'F', 'f', 'G', 'g', 'A', 'a', 'B'] 
-    base_freq = 440 #Frequency of Note A4
-    keys = np.array([x+str(y) for y in range(0,9) for x in octave])
-    # Trim to standard 88 keys
-    start = np.where(keys == 'A0')[0][0]
-    end = np.where(keys == 'C8')[0][0]
-    keys = keys[start:end+1]
-    
-    note_freqs = dict(zip(keys, [2**((n+1-49)/12)*base_freq for n in range(len(keys))]))
-    note_freqs[''] = 0.0 # stop
-    return note_freqs
-
 note_freqs = get_piano_notes()
 scale_intervals = ['A','a','B','C','c','D','d','E','F','f','G','g']
 
